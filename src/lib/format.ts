3219866767eaebd674ba879e6f4fe336c363a679
大原健太郎 <ooharakentarou@ooharakentarounoMacBook-Air.local>
2026-02-13 15:46:34 +0900

    Force JST formatting

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -1,16 +1,20 @@
 export function formatTime(date: Date) {
   return date.toLocaleTimeString("ja-JP", {
+    timeZone: "Asia/Tokyo",
     hour: "2-digit",
     minute: "2-digit"
   });
 }
 
 export function formatDate(date: Date) {
-  return date.toLocaleDateString("ja-JP");
+  return date.toLocaleDateString("ja-JP", {
+    timeZone: "Asia/Tokyo"
+  });
 }
 
 export function formatDateTime(date: Date) {
   return date.toLocaleString("ja-JP", {
+    timeZone: "Asia/Tokyo",
     month: "2-digit",
     day: "2-digit",
     hour: "2-digit",
